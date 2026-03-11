49e778f0b55aff70fc7f19b87e9611b17fda5bc0
YouthCorp <q988043@gmail.com>
2026-03-12 04:54:19 +0900

    Add Redirects www to non-www

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
       { protocol: 'https', hostname: 'media-*.api-sports.io' },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.squadcheck.xyz' }],
+        destination: 'https://squadcheck.xyz/:path*',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
